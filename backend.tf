82a0eb3b3367584deae7663f77c7bbabe5766803
ash2code <144456748+ash2code@users.noreply.github.com>
2024-04-24 19:52:35 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-
-terraform {
-  backend "s3" {
-    bucket = "aws-eks-mariogame-k8s" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
