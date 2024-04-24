666f7ca3ba24a098fefa0a6be91503d86db8a037
ash2code <144456748+ash2code@users.noreply.github.com>
2024-04-24 20:18:59 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "abhi-eks-${random_string.suffix.result}"
+  cluster_name = "ash-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
@@ -18,15 +18,13 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.7.0"
 
-  name                 = "abhi-eks-vpc"
+  name                 = "ash-eks-vpc"
   cidr                 = var.vpc_cidr
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
-  enable_dns_hostnames = true
-  enable_dns_support   = true
 
   tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
